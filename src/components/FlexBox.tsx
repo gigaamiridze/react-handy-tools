bdfb7d11e41518ded963e04f37a7590324bdb36f
Giga Amiridze <giga.amiridze@azry.com>
2023-11-15 12:24:14 +0400

    Initialized package

---
+++ src/components/FlexBox.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function FlexBox() {
+  return (
+    <div>FlexBox</div>
+  )
+}
+
+export default FlexBox;
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// Components
+export { default as FlexBox } from './components/FlexBox';
+export { default as Button } from './components/Button';
+
+// Hooks
+export { default as useFetch } from './hooks/useFetch';
