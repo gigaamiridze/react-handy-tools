748eca86bdf2070166f0451ed8602f22d491b62f
Giga Amiridze <giga.amiridze@azry.com>
2023-11-15 12:31:58 +0400

    Implemented useFetch custom hook

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function useFetch() {
-  return (
-    <div>useFetch</div>
-  )
+export interface useFetchProps {
+  apiHost: string;
+  endpoint: string;
+  param?: string | number;
+}
+
+function useFetch(props: useFetchProps) {
+  const { apiHost, endpoint, param } = props;
+  const [data, setData] = useState<any[] | null>(null);
+  const [isError, setIsError] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      setIsLoading(true);
+      const { data } = await axios.get(`${apiHost}/${endpoint}/${param && param}`);
+      setData(data);
+      setIsLoading(false);
+    } catch (error) {
+      setIsError(true);
+      setIsLoading(false);
+    }
+  }
+
+  const refetch = () => {
+    fetchData();
+  }
+
+  return { data, isLoading, isError, refetch };
 }
 
 export default useFetch;
